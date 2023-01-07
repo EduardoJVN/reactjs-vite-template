b9b835bdc41376ba31ff0147dd51311a6c8bf268
EduardoJvn <ejvn30@gmail.com>
2023-01-07 00:47:57 -0300

    template v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import React from 'react';
+import { store } from './app/store';
+import { AppThemeProvider } from './themes/AppThemeProvider';
+import App from './App';
+import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <AppThemeProvider>
+        <App />
+      </AppThemeProvider>
+    </Provider>
   </React.StrictMode>,
-)
+);
